1fd5d73115f8f7b1a930b9e539349b3b9a5d18c3
Lyx <1498781418@qq.com>
2022-08-29 22:42:11 +0800

    feat: 修改图片组件，不使用loading图片，使用div+css实现

--- src/image/demo/demo.tsx
+++ src/image/demo/demo.tsx
@@ -1,10 +1,16 @@
 import Image from "..";
 import React from "react";
+import Loading from "../../loading";
 
 export default () => {
   return (
     <div>
-      <Image src="https://t7.baidu.com/it/u=3203007717,1062852813&fm=193&f=GIF"/>
+      <Image
+        loader={<Loading />}
+        unloader={<span>加载错误</span>}
+        width={200}
+        height={200}
+        src="https://t7.baidu.com/it/u=3203007717,1062852813&fm=193&f=GIF"/>
     </div>
   )
 }
--- src/image/index.tsx
+++ src/image/index.tsx
@@ -1,18 +1,58 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 
 
 interface ImageProps {
-  src:string
+  src:string;
+  loader:any;
+  unloader:any;
+  width?:number;
+  height?:number;
 }
 
-export default function Image(props:ImageProps):JSX.Element {
-  const imgRef  = useRef<HTMLImageElement|null>(null);
-  
-  const {src} = props;
+function imgPromise(src:string):Promise<string> {
+  return new Promise((resolve, reject) => {
+    const i = new Image();
+    i.onload = () => resolve(src);
+    i.onerror = (err) => reject(err);
+    i.src = src;
+  })
+}
+
+function useImage(src:string):{isLoading:boolean, src: string, err: string} {
+
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>('');
+  const [value, setValue] = useState<string>('');
+
+  useEffect(() => {
+    imgPromise(src)
+      .then(src => {
+        setLoading(false);
+        setValue(src);
+      }).catch(err => {
+        setLoading(false);
+        setError(err);
+      })
+  }, [src]);
+
+  return {isLoading:loading, src:value, err:error}
+}
+
+
+
+export default function LImage({src, loader, unloader, ...rest}:ImageProps):JSX.Element {
+
+  const {isLoading, src:imgSrc, err} = useImage(src);
+  const width = rest.width || 50;
+  const height = rest.height || 50;
+
   return (
-    <div className="l-img-wrapper">
-      <img className="l-img" src={src} alt="" ref={imgRef}/>
+    <div className='l-img--wrapper' style={{width: width, height: height}}>
+      {imgSrc && <img className='l-img' src={imgSrc}/>}
+      {isLoading && loader}
+      {err && unloader}
     </div>
   )
+
 }
