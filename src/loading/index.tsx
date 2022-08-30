0d3d44df359d2ff69edb18c8b573c85027736c81
Lyx <1498781418@qq.com>
2022-08-30 14:36:46 +0800

    feat(loading): 可以穿入width和height改变loading大小

--- src/loading/index.tsx
+++ src/loading/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import './style.css';
 
-export default function Loading() {
+interface LoadingProps {
+  width?: number;
+  height?: number;
+}
+
+export default function Loading(props: LoadingProps) {
+  const { width, height } = props;
+  const widthSize = width || 50;
+  const heightSize = height || 50;
   return (
-    <div className='l-load'>
+    <div className='l-load' style={{width:widthSize, height:heightSize}}>
     </div>
   )
 }
